0c14e54687065df5e411f1ad8b75937e8f267d34
Tapiwanashe Shoshore <tapsshore@gmail.com>
2023-05-18 21:25:36 +0200

    first commit

---
+++ src/main/java/com/shoshore/churchback/ChurchBackApplication.java
@@ -0,0 +1,13 @@
+package com.shoshore.churchback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoshore/churchback/ChurchBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoshore.churchback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
