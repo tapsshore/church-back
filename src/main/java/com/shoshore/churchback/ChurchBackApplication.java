2c85359c4d2944b6b81f0f4d46d5c71684c52a7b
Tapiwanashe Shoshore <tapsshore@gmail.com>
2023-05-18 23:28:07 +0200

    add swagger config

--- src/main/java/com/shoshore/churchback/ChurchBackApplication.java
+++ src/main/java/com/shoshore/churchback/ChurchBackApplication.java
@@ -1,7 +1,11 @@
 package com.shoshore.churchback;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChurchBackApplication {
@@ -10,4 +14,12 @@ public class ChurchBackApplication {
         SpringApplication.run(ChurchBackApplication.class, args);
     }
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title(" Church Management Service API documentation")
+                .version("0.1")
+                .description("This is the API documentation for the Church Management service for the  application - " +
+                        " ")).addServersItem(new Server().url("http://localhost:8086")).addServersItem(new Server().url("https://test.chrch.net/api"));
+    }
 }
